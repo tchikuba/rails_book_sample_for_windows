2346aa926cafe1652ff7b91d245113ca08f61534
tchikuba <chikuba.tsutomu@gmail.com>
2017-10-13 18:31:13 +0900

    バリデーション検証用サンプルアプリにバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  validates :name, presence: true
+  validates :nickname, allow_blank: true, format: { with: /\w+/ }
+  validates :age, numericality: { only_integer: true }
+  validates :height, numericality: true
+  validates :mobile, format: { with: /\A\d{3}-\d{4}-\d{4}\z/ }
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
 end
