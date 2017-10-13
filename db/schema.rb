9ff25fc6464a40797b9d7007ffce7410399c77e2
tchikuba <chikuba.tsutomu@gmail.com>
2017-10-13 18:25:49 +0900

    rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171013072649) do
+ActiveRecord::Schema.define(version: 20171013091753) do
 
   create_table "diaries", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "nickname"
+    t.integer "age"
+    t.float "height"
+    t.string "mobile"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
